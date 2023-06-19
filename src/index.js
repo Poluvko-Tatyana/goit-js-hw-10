8843f060274b0b704882acfed38ae5318fdc956d
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-06-19 13:26:44 +0300

    7

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import './js/cat-api'
-
-// const div = document.querySelector('.cat-info');
-
-// const imgInfo = `<img src="" alt="" />
-// <h2 class="breed-name"></h2>
-// <p class="description"></p>
-// <p class="temperament"></p>`;
-
-// div.insertAdjacentHTML("afterbegin", imgInfo);
-
-// const loader = `<p class="loader">Loading...</p>`;
-// div.insertAdjacentHTML("afterend", loader);
-
-// catInfoEl.insertAdjacentHTML("beforeent", createMarkup(data))
